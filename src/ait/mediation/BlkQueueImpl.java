265fdd27414f9772d3c452dfe3da46abc67944a9
Denxak <denxak@gmail.com>
2024-10-03 10:06:06 +0200

    Synchronized

--- src/ait/mediation/BlkQueueImpl.java
+++ src/ait/mediation/BlkQueueImpl.java
@@ -1,20 +1,42 @@
 package ait.mediation;
 
+import java.util.LinkedList;
+
 public class BlkQueueImpl<T> implements BlkQueue<T> {
+    private final LinkedList<T> queue = new LinkedList<>();
+    private final int maxSize;
+
     public BlkQueueImpl(int maxSize) {
-        // TODO
-        throw new UnsupportedOperationException("Not implemented");
+        this.maxSize = maxSize;
     }
 
     @Override
-    public void push(T message) {
-        // TODO
-        throw new UnsupportedOperationException("Not implemented");
+    public synchronized void push(T message) {
+        while (queue.size() == maxSize) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                e.printStackTrace();
+            }
+        }
+        queue.addLast(message);
+        notifyAll();
     }
 
     @Override
-    public T pop() {
-        // TODO
-        throw new UnsupportedOperationException("Not implemented");
+    public synchronized T pop() {
+        while (queue.isEmpty()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                e.printStackTrace();
+                return null;
+            }
+        }
+        T res = queue.removeFirst();
+        notifyAll();
+        return res;
     }
 }
