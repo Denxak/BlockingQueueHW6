5abfcc50feecf0500fb475fcb895aae9e2d0ab73
Denxak <denxak@gmail.com>
2024-10-03 10:25:56 +0200

    Blocking Queue. LinkedBlockingQueue

--- src/ait/mediation/BlkQueueImpl.java
+++ src/ait/mediation/BlkQueueImpl.java
@@ -1,58 +1,33 @@
 package ait.mediation;
 
-import java.util.LinkedList;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class BlkQueueImpl<T> implements BlkQueue<T> {
-    private final LinkedList<T> queue = new LinkedList<>();
-    private final int maxSize;
-    private Lock mutex = new ReentrantLock();
-    private Condition notFull = mutex.newCondition();
-    private Condition notEmpty = mutex.newCondition();
+    private final BlockingQueue<T> queue;
 
     public BlkQueueImpl(int maxSize) {
-        this.maxSize = maxSize;
+        this.queue = new LinkedBlockingQueue<T>(maxSize);
     }
 
     @Override
     public void push(T message) {
-        mutex.lock();
         try {
-            while (queue.size() == maxSize) {
-                try {
-                    notFull.await();
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    e.printStackTrace();
-                }
-                queue.addLast(message);
-                notEmpty.signal();
-            }
-        } finally {
-            mutex.unlock();
+            queue.put(message);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
         }
     }
 
     @Override
     public T pop() {
-        mutex.lock();
         try {
-            while (queue.isEmpty()) {
-                try {
-                    notEmpty.await();
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    e.printStackTrace();
-                    return null;
-                }
-            }
-            T res = queue.removeFirst();
-            notFull.signal();
-            return res;
-        } finally {
-            mutex.unlock();
+            return queue.take();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+            return null;
         }
     }
 }
